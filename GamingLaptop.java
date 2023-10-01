f6b370b7311bc7e5de3d308336564ad0c33e4026
TemirlanBakytbekov <123005454+TemirlanBakytbekov@users.noreply.github.com>
2023-10-02 03:19:56 +0600

    Add files via upload

---
+++ GamingLaptop.java
@@ -0,0 +1,6 @@
+public class GamingLaptop extends Laptop{
+    public GamingLaptop(String model, String CPU, String videoCard, String RAM) {
+        super(model, CPU, videoCard, RAM);
+    }
+
+}
---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        GamingLaptop Gaminglaptop = new GamingLaptop("blablabla","Intel i5","Nvidia RTX 1650Ti","16Gb");
+        Gaminglaptop.setTurnOn(new RGB());
+        Gaminglaptop.executeTurnOn();
+    }
+}
